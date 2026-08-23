fea1a43ace742729d03babab874788adecf15585
Ahmed Anwar <ahmedanwar3189@icloud.com>
2026-08-23 05:48:48 +0500

    Connect Task App to Neon Database

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL',
-    'sqlite:///tasks.db'
-)
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 class Task(db.Model):
